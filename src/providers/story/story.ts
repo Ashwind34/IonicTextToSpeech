92af35b81a3475c1a858a0cd8b84162f4c07b229
Ashwind34 <ahester34@gmail.com>
2019-05-20 16:19:54 -0700

    story buttons working.  need to fix cors on server side before testing

--- src/providers/story/story.ts
+++ src/providers/story/story.ts
@@ -13,6 +13,8 @@ export class StoryProvider {
   seed: number;
 
   gptResponse: any;
+  
+  fakeResponse: string = 'This is the fake response'
 
   params: any = {
     "length": this.length,
@@ -25,26 +27,28 @@ export class StoryProvider {
   }
 
   randomStory() {
-    return this.http.get(this.getUrl)
-    .subscribe(
-      (response: any) => {
-        this.gptResponse = response;
-        console.log(this.gptResponse);
-      }, error => {
-        console.log('Error Status Code: ' + error.status + ' (' + error.statusText + ')')
-      }
-    );
+    console.log(this.fakeResponse);
+    // return this.http.get(this.getUrl)
+    // .subscribe(
+    //   (response: any) => {
+    //     this.gptResponse = response;
+    //     console.log(this.gptResponse);
+    //   }, error => {
+    //     console.log('Error Status Code: ' + error.status + ' (' + error.statusText + ')')
+    //   }
+    // );
   }
 
   userStory(){
-    return this.http.post(this.postUrl, this.params)
-    .subscribe(
-      (response: any) => {
-        this.gptResponse = response;
-      }, error => {
-        console.log('Error Status Code: ' + error.status + ' (' + error.statusText + ')')
-      }
-    );
+    console.log(this.fakeResponse);
+    // return this.http.post(this.postUrl, this.params)
+    // .subscribe(
+    //   (response: any) => {
+    //     this.gptResponse = response;
+    //   }, error => {
+    //     console.log('Error Status Code: ' + error.status + ' (' + error.statusText + ')')
+    //   }
+    // );
   }
 
   

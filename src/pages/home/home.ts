6a440fa64460e4a9b673cc9d07eb07483b7153c6
Ashwind34 <ahester34@gmail.com>
2019-05-22 16:46:38 -0700

    multiple style changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { StoryProvider } from '../../providers/story/story';
 import { StorybookPage } from '../storybook/storybook';
 
